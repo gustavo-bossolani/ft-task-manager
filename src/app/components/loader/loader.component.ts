dd53a87748164389b71892e4097ff42842ccbc86
Gustavo Bossolani <gustavo.wyd@outlook.com>
2021-03-02 01:09:59 -0300

    cria componente de loader

---
+++ src/app/components/loader/loader.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.css']
+})
+export class LoaderComponent implements OnInit {
+
+  @Input()
+  type: 'small' | 'medium'| 'large' = 'medium';
+
+  componentSettings = {
+    smallest: {
+      border: '3px solid #f3f3f3',
+      border_top: '3px solid #ef4444',
+      width: '15px',
+      height: '15px'
+    },
+
+    small: {
+      border: '5px solid #f3f3f3',
+      border_top: '5px solid #ef4444',
+      width: '40px',
+      height: '40px'
+    },
+    medium: {
+      border: '8px solid #f3f3f3',
+      border_top: '8px solid #ef4444',
+      width: '80px',
+      height: '80px'
+    },
+    large: {
+      border: '10px solid #f3f3f3',
+      border_top: '10px solid #ef4444',
+      width: '110px',
+      height: '110px'
+    },
+  };
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+}
