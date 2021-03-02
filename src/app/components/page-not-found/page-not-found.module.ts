481516c6ba5b94add547e813d02aab0e7d824c28
Gustavo Bossolani <gustavo.wyd@outlook.com>
2021-03-02 01:13:54 -0300

    cria componente de page not found

---
+++ src/app/components/page-not-found/page-not-found.module.ts
@@ -0,0 +1,16 @@
+import { PageNotFoundComponent } from './page-not-found.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    PageNotFoundComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    PageNotFoundComponent
+  ]
+})
+export class PageNotFoundModule { }
